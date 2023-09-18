f105d8e0e8683d05056c010547590618ae999ba1
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-09-18 15:57:32 -0300

    changed controller to use async

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,18 +1,30 @@
 const { Thought, User } = require("../models");
 
-const thoughtController {
+module.exports = {
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find()
+            .populate({ path: "reactions", select: '-___v'});
 
-    //get Thoughts
+            res.json(thoughts);
+            } catch (err) {
+            console.error({ message: err });
+            res.status(500).json(err);
+        }
+    },
+    
+    async getThoughtById(req,res) {
+        try {
+            const thought = await thought.findOne({_id: req.params.postId })
+            .populate({ path: "reactions", select: '-___v'});
 
-    //get Thought by Id
+            if (!thought) {
+                return res.status(400).json({ message: 'No post with that ID' });
+            }
 
-    //create Thought
-
-    //update Thought
-
-    //delete Thought
-
-
-};
-
-module.exports = thoughtController;
+            res.json(thought);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }
+}
