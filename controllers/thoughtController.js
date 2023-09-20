f286f73e3dd83946a989c5523806fb07a42f4320
Chris <130914528+ChrisSutt@users.noreply.github.com>
2023-09-20 11:03:47 -0300

    added createThought section in thoughtController

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -27,4 +27,25 @@ module.exports = {
             res.status(500).json(err);
         }
     }
-}
+},
+
+
+async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      const user = await User.findOneAndUpdate(
+        { _id: req.body.userId },
+        { $push: { thoughts: thought._id } },
+        { new: true }
+      );
+
+      if (!user) {
+        return res.status(404).json({ message: "User not found!" });
+      }
+
+      res.json({ message: "Thought successfully created!" });
+    } catch (err) {
+      console.error({ message: err });
+      res.status(500).json(err);
+    }
+  },
